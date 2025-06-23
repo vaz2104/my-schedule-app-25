e57910a2a4ccb2daf34d6f6a4b3bbb5aa3f39a95
vaz2104 <s.manuliak@gmail.com>
2025-06-23 14:18:59 +0300

    loading components

---
+++ src/app/components/ui/FullScreenLoader.js
@@ -0,0 +1,9 @@
+import Spinner from "./Spinner";
+
+export default function FullScreenLoader() {
+  return (
+    <div className="ms-full-creen flex justify-center items-center">
+      <Spinner />
+    </div>
+  );
+}

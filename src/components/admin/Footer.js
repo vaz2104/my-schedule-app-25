4b631be20d310c5b454abfd494190246221c127f
vaz2104 <s.manuliak@gmail.com>
2025-08-21 12:32:13 +0300

    added a base link to the footer nav

--- src/components/admin/Footer.js
+++ src/components/admin/Footer.js
@@ -16,39 +16,48 @@ export default function Footer() {
   return (
     <div className="fixed bottom-0 left-0 z-20 w-full px-4 py-4 bg-white">
       <div className="footer-nav">
-        <Link href={`/dashboard/3/`} className="footer-nav-item">
+        <Link href={`${baseDashboardLink}/`} className="footer-nav-item">
           <div className="footer-nav-item__icon">
             <HomeIcon className={"size-8"} />
           </div>
           <div className="footer-nav-item__label">Головна</div>
         </Link>
-        <Link href={`/dashboard/3/specialists`} className="footer-nav-item">
+        <Link
+          href={`${baseDashboardLink}/specialists`}
+          className="footer-nav-item"
+        >
           <div className="footer-nav-item__icon">
             {/* <UsersGroupIcon className={"size-9"} /> */}
             <CalendarIcon className={"size-8"} />
           </div>
           <div className="footer-nav-item__label">Працівники</div>
         </Link>
-        <Link href={`/dashboard/3/clients`} className="footer-nav-item">
+        <Link href={`${baseDashboardLink}/clients`} className="footer-nav-item">
           <div className="footer-nav-item__icon">
             <UsersGroupIcon className={"size-9"} />
             {/* <CalendarIcon className={"size-8"} /> */}
           </div>
           <div className="footer-nav-item__label">Клієнти</div>
         </Link>
-        <Link href={`/dashboard/3/services`} className="footer-nav-item">
+        <Link
+          href={`${baseDashboardLink}/services`}
+          className="footer-nav-item"
+        >
           <div className="footer-nav-item__icon">
             <BriefCaseIcon className={"size-7"} />
           </div>
           <div className="footer-nav-item__label">Послуги</div>
         </Link>
-        <Link href={`/dashboard/3/events`} className="footer-nav-item">
+        <Link href={`${baseDashboardLink}/events`} className="footer-nav-item">
           <div className="footer-nav-item__icon">
             <BellIcon className={"size-8"} />
           </div>
           <div className="footer-nav-item__label">Події</div>
         </Link>
-        <Link href={`/dashboard/3/settings`} className="footer-nav-item">
+        <Link
+          href={`${baseDashboardLink}/settings`}
+          className="footer-nav-item"
+        >
           <div className="footer-nav-item__icon">
             <UserSettingsIcon className={"size-8"} />
           </div>

decacdbb672fdcd939eee98648d5437ded123204
vaz2104 <s.manuliak@gmail.com>
2025-08-24 21:53:32 +0300

    refactored format date function

--- src/lib/formatDate.js
+++ src/lib/formatDate.js
@@ -1,11 +1,15 @@
-export default function formatDate(date) {
+export default function formatDate(date, type = "code") {
   if (!date) return "";
   const dateObject = typeof date === "object" ? date : new Date(date);
   const year = dateObject.getFullYear();
   const month = dateObject.getMonth() + 1;
   const day = dateObject.getDate();
 
-  return `${day < 10 ? "0" + day : day}-${
-    month < 10 ? "0" + month : month
-  }-${year}`;
+  return type === "ui"
+    ? `${day < 10 ? "0" + day : day}-${
+        month < 10 ? "0" + month : month
+      }-${year}`
+    : `${year}-${month < 10 ? "0" + month : month}-${
+        day < 10 ? "0" + day : day
+      }`;
 }

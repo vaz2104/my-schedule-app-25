2dc2894bfd30243d01badfcd498ca30c4cd80f28
vaz2104 <s.manuliak@gmail.com>
2025-09-27 23:06:20 +0300

    added an app store

---
+++ src/store/useAppStore.js
@@ -0,0 +1,10 @@
+import { create } from "zustand";
+
+export const useAppStore = create((set) => ({
+  companyPlan: null,
+  botName: "",
+  themePalette: "",
+  setCompanyPlan: (plan) => set(() => ({ companyPlan: plan })),
+  setbBotName: (name) => set(() => ({ botName: name })),
+  setThemePalette: (palette) => set(() => ({ themePalette: palette })),
+}));

57bdaa56a4e8746e44d3f7a3dd4fd943066d39ac
vaz2104 <s.manuliak@gmail.com>
2025-10-26 18:07:01 +0200

    added a warning to the workers list

--- src/app/dashboard/[companyID]/specialists/page.js
+++ src/app/dashboard/[companyID]/specialists/page.js
@@ -5,10 +5,12 @@ import Alert from "@/components/ui/Alert";
 import Spinner from "@/components/ui/Spinner";
 import { useBaseURL } from "@/hooks/useBaseURL";
 import { CompanyService } from "@/services/CompanyService";
+import { useAppStore } from "@/store/useAppStore";
 import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
 
 export default function Specialists() {
+  const { companyPlan } = useAppStore();
   const [workers, setWorkers] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
@@ -28,6 +30,23 @@ export default function Specialists() {
     }
 
     setIsLoading(false);
+  }
+
+  function checkLimits() {
+    let isOverLimit = false;
+    switch (companyPlan) {
+      case "free":
+        isOverLimit = workers.length === 1;
+        break;
+      case "basic":
+        isOverLimit = workers.length === 1;
+        break;
+      case "business":
+        isOverLimit = workers.length === 3;
+        break;
+    }
+
+    return isOverLimit;
   }
 
   useEffect(() => {
@@ -53,8 +72,27 @@ export default function Specialists() {
       <div className="mb-8 mt-4 text-center">
         <h2 className="font-bold text-xl">Працівники</h2>
       </div>
+      {
+        <div className="my-4">
+          <Alert type="default">
+            <div className="">
+              <p>Ви більше не можете додавати працівників!</p>
+              <p>
+                Ваш тарифний план <span className="font-bold">"Business"</span>,
+                максимальна кількість працівників в цьому плані -{" "}
+                <span className="font-bold">3</span>
+              </p>
+              <p>
+                Аби додати більше працівників заблокуйте, чи видаліть наявних,
+                або змініть тарифний план на{" "}
+                <span className="font-bold">"Business Plus"</span>
+              </p>
+            </div>
+          </Alert>
+        </div>
+      }
       <div className="mb-6">
-        <InviteWorker />
+        <InviteWorker isButtonDisabled={checkLimits()} />
       </div>
       <div>
         <WorkersList workers={workers} baseURL={baseDashboardLink} />

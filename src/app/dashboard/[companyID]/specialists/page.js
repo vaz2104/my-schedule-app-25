62cae3194464e7f271dd124c9f775ff7bf0976e3
vaz2104 <s.manuliak@gmail.com>
2025-11-23 21:53:14 +0200

    changed message of workers limits

--- src/app/dashboard/[companyID]/specialists/page.js
+++ src/app/dashboard/[companyID]/specialists/page.js
@@ -6,6 +6,7 @@ import Spinner from "@/components/ui/Spinner";
 import { useBaseURL } from "@/hooks/useBaseURL";
 import { CompanyService } from "@/services/CompanyService";
 import { useAppStore } from "@/store/useAppStore";
+import Link from "next/link";
 import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
 
@@ -34,19 +35,65 @@ export default function Specialists() {
 
   function checkLimits() {
     let isOverLimit = false;
-    switch (companyPlan) {
-      case "free":
-        isOverLimit = workers.length === 1;
-        break;
-      case "basic":
-        isOverLimit = workers.length === 1;
-        break;
-      case "business":
-        isOverLimit = workers.length === 3;
-        break;
+
+    const planPositions =
+      companyPlan === "basic" || companyPlan === "free"
+        ? 1
+        : companyPlan === "business"
+        ? 3
+        : 10000;
+
+    let activeWorkers = 0;
+
+    workers.map((workerItem) => {
+      if (!workerItem?.isBlocked) activeWorkers += 1;
+    });
+
+    const availablePositions = planPositions - activeWorkers;
+
+    if (availablePositions <= 0) {
+      isOverLimit = true;
     }
 
     return isOverLimit;
+  }
+
+  function workersNumber(plan) {
+    let wokersNumber = 1;
+
+    if (plan == "business") wokersNumber = 3;
+    if (plan == "businessPlus") wokersNumber = "більше 3-x";
+
+    return wokersNumber;
+  }
+
+  function planLabel(plan) {
+    let title = "";
+
+    switch (plan) {
+      case "free":
+        title = "Безкоштовний";
+        break;
+      case "basic":
+        title = "Базовий";
+        break;
+      case "business":
+        title = "Бізнес";
+        break;
+      case "businessPlus":
+        title = "Бізнес Plus";
+        break;
+    }
+
+    return title;
+  }
+
+  function nextPlan(plan) {
+    const plans = ["basic", "business", "businessPlus"];
+    const activePlanIndex = plans.indexOf(plan);
+    if (activePlanIndex != -1) {
+      return planLabel(plans[activePlanIndex + 1]);
+    }
   }
 
   useEffect(() => {
@@ -68,32 +115,44 @@ export default function Specialists() {
     );
   }
   return (
-    <div className="p-4">
+    <div className="p-4 block">
       <div className="mb-8 mt-4 text-center">
         <h2 className="font-bold text-xl">Працівники</h2>
       </div>
       {checkLimits() && (
         <div className="my-4">
-          <Alert type="default">
+          <Alert type="warning">
             <div className="">
-              <p>Ви більше не можете додавати працівників!</p>
-              <p>
+              <p>Досягнуто ліміту активних працівників!</p>
+              <p className="mt-1.5">
                 Ваш тарифний план{" "}
-                <span className="font-bold">&quot;Business&quot;</span>,
-                максимальна кількість працівників в цьому плані -{" "}
-                <span className="font-bold">3</span>
+                <span className="font-bold">
+                  &quot;{planLabel(companyPlan)}&quot;
+                </span>
+                , максимальна кількість активних працівників в цьому плані -{" "}
+                <span className="font-bold">{workersNumber(companyPlan)}</span>
               </p>
-              <p>
+              <p className="mt-1.5">
                 Аби додати більше працівників заблокуйте, чи видаліть наявних,
                 або змініть тарифний план на{" "}
-                <span className="font-bold">&quot;Business Plus&quot;</span>
+                <span className="font-bold">
+                  &quot;{nextPlan(companyPlan)}&quot;
+                </span>
+              </p>
+              <p className="mt-1.5">
+                <Link
+                  href={`${baseDashboardLink}/pricing`}
+                  className="underline"
+                >
+                  Переглянути тарифні плани
+                </Link>
               </p>
             </div>
           </Alert>
         </div>
       )}
       <div className="mb-6">
-        <InviteWorker isButtonDisabled={checkLimits()} />
+        <InviteWorker isButtonDisabled={isLoading} />
       </div>
       <div>
         <WorkersList workers={workers} baseURL={baseDashboardLink} />

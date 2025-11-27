427f94b06819479d86bcb5a6f85d11ba202d0efd
vaz2104 <s.manuliak@gmail.com>
2025-11-27 06:42:26 +0200

    clear session on launch for telegram users

--- src/components/general/_login/LoginLayout.js
+++ src/components/general/_login/LoginLayout.js
@@ -3,14 +3,20 @@ import { useEffect, useState } from "react";
 import TelegramAuthorization from "./TelegramAuthorization";
 import FormAuthorization from "./FormAuthorization";
 import FullScreenLoader from "../../ui/FullScreenLoader";
+import { AuthService } from "@/services/AuthService";
 
 export default function LoginLayout() {
   const [telegramUserID, setTelegramUserID] = useState(null);
   const [isLoader, setIsLoader] = useState(true);
 
-  function checkTelegramUser() {
+  async function checkTelegramUser() {
     if (typeof window !== "undefined") {
       const userID = window.Telegram?.WebApp?.initDataUnsafe?.user?.id;
+
+      if (userID) {
+        await AuthService.destroySession();
+        localStorage.removeItem("activePanel");
+      }
 
       setTelegramUserID(userID);
       setIsLoader(false);

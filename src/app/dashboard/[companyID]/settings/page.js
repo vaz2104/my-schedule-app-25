dde183a4655bc86fae8c4a81b9bc1a6823219d09
vaz2104 <s.manuliak@gmail.com>
2025-12-11 22:58:58 +0200

    bot status settings

--- src/app/dashboard/[companyID]/settings/page.js
+++ src/app/dashboard/[companyID]/settings/page.js
@@ -41,8 +41,48 @@ export default function SettingsPage() {
       </div>
 
       <div className="mb-12">
-        <div className="mb-4">
+        <div className="mb-1">
           <h2 className="font-bold text-lg">Налаштування видимості</h2>
+        </div>
+        <div className="mb-4 m-auto">
+          <p className="text-gray-500 text-base">
+            Ви можете тимчасово обмежити доступ клієнтів до Вашого графіку
+          </p>
+        </div>
+        <div className="py-6 bg-gray-50 p-4 pb-8">
+          <label className="flex items-center cursor-pointer">
+            <span className="block mr-3 text-base font-medium text-gray-600 dark:text-white flex-1">
+              Закрити бот для відвідувачів
+            </span>
+            <input
+              type="checkbox"
+              // value={appointmentReminderHintEnabled}
+              // onChange={() =>
+              //   updateClientSettings(
+              //     "appointmentReminderHintEnabled",
+              //     !appointmentReminderHintEnabled
+              //   )
+              // }
+              className="sr-only peer"
+              // checked={appointmentReminderHintEnabled}
+            />
+            <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-main "></div>
+          </label>
+
+          <div className="mt-4">
+            <label htmlFor="password" className="input-label">
+              Повідомлення, яке будуть бачити користувачі
+            </label>
+            <textarea
+              type="text"
+              id="password"
+              placeholder="Введіть повідомлення..."
+              className="input !bg-white"
+              rows="4"
+              // onChange={(e) => setPass(e.target.value)}
+              // value={pass}
+            ></textarea>
+          </div>
         </div>
       </div>
 

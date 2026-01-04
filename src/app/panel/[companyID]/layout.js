5636f34dc552ee540ed790a8b6d9a80ce4653899
vaz2104 <s.manuliak@gmail.com>
2026-01-04 22:17:09 +0200

    hide header for panel

--- src/app/panel/[companyID]/layout.js
+++ src/app/panel/[companyID]/layout.js
@@ -4,8 +4,8 @@ import Header from "@/components/client/Header";
 export default function DashboardCompanyLayout({ children }) {
   return (
     <div className="ms-full-creen relative z-10">
-      <Header />
-      <main className="flex-1 pt-30 pb-22">{children}</main>
+      {/* <Header /> */}
+      <main className="flex-1 pb-22">{children}</main>
       <Footer />
     </div>
   );

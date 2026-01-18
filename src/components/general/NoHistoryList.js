5c35a17ec191b9160af115cf67f0ca404fd376b0
vaz2104 <s.manuliak@gmail.com>
2026-01-18 22:32:15 +0200

    refactored no history block

--- src/components/general/NoHistoryList.js
+++ src/components/general/NoHistoryList.js
@@ -1,16 +1,11 @@
 import Image from "next/image";
+import poster from "/public/no-history.svg";
 
 export default function NoHistoryList() {
   return (
     <div className="mt-10">
       <div className="flex justify-center">
-        <Image
-          src={"/no-history-poster-modified.png"}
-          width={320}
-          height={320}
-          alt=""
-          className="max-w-xs"
-        />
+        <Image src={poster} alt="my icon" width={320} height={320} />
       </div>
 
       <p className="my-8 text-lg text-center text-gray-400 ">

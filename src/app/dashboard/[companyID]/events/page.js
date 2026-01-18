5807db708589181453b56f4d2cde795c95c2df72
vaz2104 <s.manuliak@gmail.com>
2026-01-18 22:33:11 +0200

    fixed events page

--- src/app/dashboard/[companyID]/events/page.js
+++ src/app/dashboard/[companyID]/events/page.js
@@ -57,7 +57,7 @@ export default function Events() {
         <h2 className="font-bold text-xl">Події</h2>
       </div>
       <div>
-        {events?.length ? (
+        {!events?.length ? (
           <NoNotifications />
         ) : (
           <>

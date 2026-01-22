c01deded4f2633335e06cf43eca3f02bc1d867a0
vaz2104 <s.manuliak@gmail.com>
2026-01-22 12:04:35 +0200

    removed unused method

--- src/services/ClientService.js
+++ src/services/ClientService.js
@@ -1,21 +1,6 @@
 import fetchApi from "../lib/fetchApi";
 
 class ClientServiceClass {
-  async getClientRelation(options) {
-    let sp = new URLSearchParams(options);
-    const queryString = sp.toString();
-
-    const data = await fetchApi(
-      `${process.env.NEXT_PUBLIC_BOT_BACKEND_URL}/api/company/client-relation/${
-        queryString ? `?${queryString}` : ``
-      }`,
-      {},
-      "GET"
-    );
-
-    return data;
-  }
-
   async getSingle(id) {
     if (!id) return null;
 

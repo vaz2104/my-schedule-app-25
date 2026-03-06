c94b16e284072ffbb4b96f052b5f62d0285beb60
vaz2104 <s.manuliak@gmail.com>
2026-03-06 23:02:22 +0200

    refatored baseURL hook

--- src/hooks/useBaseURL.js
+++ src/hooks/useBaseURL.js
@@ -3,15 +3,18 @@ import { useEffect, useState } from "react";
 
 export const useBaseURL = () => {
   const [activePanel, setActivePanel] = useState("");
+  const [baseDashboardLink, setBaseDashboardLink] = useState("");
+  const [basePlatformLink, bsetBasePlatformLink] = useState("");
 
   useEffect(() => {
     if (typeof window !== "undefined") {
-      setActivePanel(localStorage.getItem("activePanel"));
+      const panelId = localStorage.getItem("activePanel");
+
+      setActivePanel(panelId);
+      setBaseDashboardLink(`/dashboard/${panelId}`);
+      bsetBasePlatformLink(`/panel/${panelId}`);
     }
   }, []);
-
-  const baseDashboardLink = `/dashboard/${activePanel}`;
-  const basePlatformLink = `/panel/${activePanel}`;
 
   return { baseDashboardLink, basePlatformLink, activePanel };
 };
